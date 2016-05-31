0b4107b165ee4891cc0edebbc7a7a5574f1a82d1
Propietari <ev3c.android@gmail.com>
2016-06-01 00:43:29 +0200

    FROM LENOVO

--- app/src/main/java/com/android/ev3c/avisos/AvisosActivity.java
+++ app/src/main/java/com/android/ev3c/avisos/AvisosActivity.java
@@ -30,6 +30,7 @@ public class AvisosActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // REM FROM LENOVO
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_avisos, menu);
         return true;
